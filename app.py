2122f56a61ccb482012c006092b9e708ba91e718
Pavlo Hrechko <pavlo@undagrid.com>
2025-12-01 21:34:14 +0100

    reqa

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def confirm_mapping():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
